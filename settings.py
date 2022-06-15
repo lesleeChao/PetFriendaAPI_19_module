1119e8d1d8e2beb56056d1c8ad8c11aa8fbbc901
BATO <95556815+lesleeChao@users.noreply.github.com>
2022-06-15 13:19:15 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-valid_email = "choynzhurov97@bk.ru"
-valid_password = "20Lowella0100"
+valid_email = "choin97@bk.ru"
+valid_password = "20Lowella00000"
 
 non_existent_email = "qwerty@mail.ru"
 non_existent_pass = "12345678"
